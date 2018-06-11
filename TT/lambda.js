510976ba17b5f1c215fc5cafb229b54efd07cc06
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-11 14:29:22 +0530

    Adding status codes

--- TT/lambda.js
+++ TT/lambda.js
@@ -1,32 +1,43 @@
 let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const ddb = new AWS.DynamoDB.DocumentClient();
+const validateJS = require('validate.js');
 exports.handler = function (event, context, callback) {
 
-	console.log("Request received", event);
+	let image = Buffer.from(event.image.replace("data:image/jpeg;base64,", ""), "base64");
 
-	let image = Buffer.from(event.image, "base64");
+
+	let validation = validateJS(event, { age: { numericality: true } });
+
+	if (validation) {
+		console.error("Validation failure");
+		callback(null,JSON.stringify(validation));
+	}
+
 
 	ddb.put({
 		TableName: 'mypeople',
 		Item: { 'username': event.username, 'firstName': event.firstName, 'lastName': event.lastName, 'age': event.age }
 	}, function (err, data) {
-		s3.putObject({
-			"Body": image,
-			"Bucket": "testxyz.abc.slapp.food",
-			"Key": event.username,
-			"ACL": "public-read"
-		})
-			.promise()
-			.then(data => {
-				console.log(data);           // successful response
-				callback(null, data);
+		if (err) {
+			callback("Error in saving to db", null);
+		} else {
+			s3.putObject({
+				"Body": image,
+				"Bucket": "testxyz.abc.slapp.food",
+				"Key": event.username + ".jpg",
+				"ACL": "public-read"
 			})
-			.catch(err => {
-				console.log(err, err.stack); // an error occurred
-				callback(err, null);
-			});
-
+				.promise()
+				.then(data => {
+					console.log(data);           // successful response
+					callback(null, data);
+				})
+				.catch(err => {
+					console.log(err, err.stack); // an error occurred
+					callback(err, null);
+				});
+		}
 	});
 
 }
--- TT/list.js
+++ TT/list.js
@@ -15,14 +15,14 @@ exports.handler = function (event, context, callback) {
 		TableName: 'mypeople',
 		Key: { 'username': event.queryStringParameters.username }
 	}, function (err, data) {
-
 		if (err) {
 			response.statusCode = 500;
 			response.body = "Error in fetching user with username";
 		} else {
 			if (data.Item) {
+				data.Item.image = process.env["IMAGE_BUCKET"] + "/" + data.Item.username + ".jpg";
 				response.body = JSON.stringify(data.Item);
-			}else{
+			} else {
 				response.statusCode = 404;
 				response.body = "No such user";
 			}
